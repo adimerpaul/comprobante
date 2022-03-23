fdb8880915728275267a6569a1b4b7f2b159f755
adimerpaul <adimer101@gmail.com>
2022-03-23 15:18:26 -0400

    Update Pagourbano.vue

--- comprobantefront/src/pages/Pagourbano.vue
+++ comprobantefront/src/pages/Pagourbano.vue
@@ -440,15 +440,19 @@ export default {
       let sumtotal=0
       this.miscomprobantes.forEach(r=>{
         // xx+=0.5
-        y+=0.5
-        console.log(r)
-        doc.text(1, y+3, r.nrocomprobante)
-        doc.text(4, y+3, r.nrotramite)
-        doc.text(7, y+3, r.cliente.paterno+' '+r.cliente.materno+' '+r.cliente.nombre)
-        doc.text(13.5, y+3, r.cliente.ci)
-        doc.text(16, y+3, r.total)
-        sumtotal+=parseInt(r.total)
-        doc.text(18, y+3, r.user.codigo )
+
+        // console.log(r)
+        if(r.nrocomprobante!=''){
+          y+=0.5
+          doc.text(1, y+3, r.nrocomprobante)
+          doc.text(4, y+3, r.nrotramite)
+          doc.text(7, y+3, r.cliente.paterno+' '+r.cliente.materno+' '+r.cliente.nombre)
+          doc.text(13.5, y+3, r.cliente.ci)
+          doc.text(16, y+3, r.total)
+          sumtotal+=parseInt(r.total)
+          doc.text(18, y+3, r.user.codigo )
+        }
+
         // if (y+3>25){
         //   doc.addPage();
         //   header(this.fecha)
